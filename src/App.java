af6842f8c5a0c08495447c74b96d9803178a0fd2
Iker <ikerhf14@gmail.com>
2026-06-02 13:37:41 +0100

    Ejercicio 1: añadidos pedidos de ejemplo a la cola

--- src/App.java
+++ src/App.java
@@ -6,6 +6,16 @@ public class App {
     public static void main(String[] args) {
 
         Queue<Pedido> colaPedidos = new LinkedList<>();
+        Pedido p1 = new Pedido("P001", "Carla", "Café con leche", true);
+        Pedido p2 = new Pedido("P002", "Mario", "Bocadillo de pollo", false);
+        Pedido p3 = new Pedido("P003", "Nerea", "Zumo natural", true);
+        Pedido p4 = new Pedido("P004", "Iván", "Tostada integral", false);
+        Pedido p5 = new Pedido("P005", "Laura", "Té verde", true);
 
+        colaPedidos.add(p1);
+        colaPedidos.add(p2);
+        colaPedidos.add(p3);
+        colaPedidos.add(p4);
+        colaPedidos.add(p5);
     }
 }
