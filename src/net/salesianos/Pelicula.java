508231838f1bac6739152c4d65e9a7a5f0cd1e52
Iker <ikerhf14@gmail.com>
2026-06-02 13:58:58 +0100

    Ejercicio 3: creada clase Pelicula

---
+++ src/net/salesianos/Pelicula.java
@@ -0,0 +1,30 @@
+package net.salesianos;
+
+public class Pelicula {
+
+    private String titulo;
+    private String genero;
+    private int duracionMinutos;
+
+    public Pelicula(String titulo, String genero, int duracionMinutos) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.duracionMinutos = duracionMinutos;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public int getDuracionMinutos() {
+        return duracionMinutos;
+    }
+
+    public String toString() {
+        return titulo + " - " + genero + " - " + duracionMinutos + " min";
+    }
+}
